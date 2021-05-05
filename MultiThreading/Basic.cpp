3d0249f1849bc23b92a127509bac072d99a0a690
Shubham Kumar <skshubham2014@gmail.com>
2021-05-05 13:37:05 +0530

    Create Basic.cpp
    Basic multithreading

---
+++ MultiThreading/Basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void threadFunc() {
+	  cout << "Executing threadFunc";
+}
+
+int main() {
+    thread t(threadFunc);
+    t.join();
+	  return 0;
+}
